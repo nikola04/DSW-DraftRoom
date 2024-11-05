0ad72dffb6bf9e8a9c012c6ec16acd933b5a37a7
SaloJovanovic <aleksandarjovanovic670@gmail.com>
2024-11-05 11:31:21 +0100

    Added subscriber for MainFrame

--- src/main/java/raf/draft/dsw/gui/swing/MainFrame.java
+++ src/main/java/raf/draft/dsw/gui/swing/MainFrame.java
@@ -1,10 +1,12 @@
 package raf.draft.dsw.gui.swing;
 
+import raf.draft.dsw.controller.observer.ISubscriber;
+import raf.draft.dsw.model.messages.Message;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class MainFrame extends JFrame {
-    //buduca polja za sve komponente view-a na glavnom prozoru
+public class MainFrame extends JFrame implements ISubscriber {
     private static MainFrame instance;
     private MainFrame() {
         initialize();
@@ -29,5 +31,28 @@ public class MainFrame extends JFrame {
 
         MyToolBar toolBar = new MyToolBar();
         add(toolBar, BorderLayout.NORTH);
+    }
+
+    @Override
+    public void update(Message message) {
+        String messageType = message.getType().toString();
+        String messageText = message.toString();
+
+        int messageTypeJOptionPane;
+        switch (message.getType()) {
+            case ERROR:
+                messageTypeJOptionPane = JOptionPane.ERROR_MESSAGE;
+                break;
+            case WARNING:
+                messageTypeJOptionPane = JOptionPane.WARNING_MESSAGE;
+                break;
+            case INFO:
+                messageTypeJOptionPane = JOptionPane.INFORMATION_MESSAGE;
+                break;
+            default:
+                messageTypeJOptionPane = JOptionPane.PLAIN_MESSAGE;
+        }
+
+        JOptionPane.showMessageDialog(this, messageText, messageType, messageTypeJOptionPane);
     }
 }
