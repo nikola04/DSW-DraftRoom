749a5d2b517a338d526064513e3262d806d89adc
SaloJovanovic <aleksandarjovanovic670@gmail.com>
2024-12-11 00:26:20 +0100

    DoorPainter & ToiletPainter fixed

--- src/main/java/raf/draft/dsw/gui/swing/view/painters/DoorPainter.java
+++ src/main/java/raf/draft/dsw/gui/swing/view/painters/DoorPainter.java
@@ -16,12 +16,20 @@ public class DoorPainter extends ElementPainter {
         AffineTransform originalTransform = g2d.getTransform();
         applyTransformations(g2d);
 
-        int centerX = element.getX();
-        int centerY = element.getY();
+        int x = element.getX();
+        int y = element.getY();
         int width = element.getWidth();
         int height = element.getHeight();
 
-        g2d.drawArc(centerX, centerY, width, height, 0, 180); // Polukrug
+        int arcDiameter = Math.min(width, height) * 2;
+        g2d.drawArc(x - (arcDiameter / 2) / 2, y - (arcDiameter / 2) / 2, arcDiameter, arcDiameter, 90, 90);
+
+        int xStart = x + width / 2;
+        int yStart = y - (arcDiameter / 2) / 2;
+        int xEnd = xStart;
+        int yEnd = y + height / 2;
+
+        g2d.drawLine(xStart, yStart, xEnd, yEnd);
 
         applySelectionBorder(g2d);
         g2d.setTransform(originalTransform);
--- src/main/java/raf/draft/dsw/gui/swing/view/painters/ToiletPainter.java
+++ src/main/java/raf/draft/dsw/gui/swing/view/painters/ToiletPainter.java
@@ -22,32 +22,17 @@ public class ToiletPainter extends ElementPainter {
         int height = element.getHeight();
 
         // Gornji pravougaonik
-        int rectHeight = height / 3; // Pravougaonik zauzima 1/3 visine
+        int rectHeight = height / 3;
         g2d.drawRect(x, y, width, rectHeight);
 
-        // Donji luk
+        // Donji luk (spoljašnji)
         int ovalWidth = width;
-        int ovalHeight = (2 * height) / 3; // Luk zauzima 2/3 visine
+        int ovalHeight = (2 * height) / 3;
         int ovalX = x;
-        int ovalY = y + rectHeight; // Počinje odmah ispod pravougaonika
-        g2d.drawArc(ovalX, ovalY, ovalWidth, ovalHeight, 0, -180); // Polukrug
+        int ovalY = y + rectHeight;
+        g2d.drawArc(ovalX, ovalY, ovalWidth, ovalHeight, 0, -180);
 
-        // Spajajuće linije
-        // Leva linija
-        int startX1 = x; // Leva ivica pravougaonika
-        int startY1 = y + rectHeight; // Donja leva tačka pravougaonika
-        int endX1 = x; // Leva tačka luka
-        int endY1 = ovalY; // Gornja leva tačka luka
-        g2d.drawLine(startX1, startY1, endX1, endY1);
-
-        // Desna linija
-        int startX2 = x + width; // Desna ivica pravougaonika
-        int startY2 = y + rectHeight; // Donja desna tačka pravougaonika
-        int endX2 = x + width; // Desna tačka luka
-        int endY2 = ovalY; // Gornja desna tačka luka
-        g2d.drawLine(startX2, startY2, endX2, endY2);
-
-        // Unutrašnji manji luk (grb)
+        // Unutrašnji manji luk
         int innerMargin = 10;
         int innerOvalWidth = ovalWidth - 2 * innerMargin;
         int innerOvalHeight = ovalHeight - 2 * innerMargin;
@@ -55,7 +40,43 @@ public class ToiletPainter extends ElementPainter {
         int innerOvalY = ovalY + innerMargin;
         g2d.drawArc(innerOvalX, innerOvalY, innerOvalWidth, innerOvalHeight, 0, -180);
 
+        // Leva linija za spoljašnji luk
+        int startX1 = x;
+        int startY1 = y + rectHeight;
+        int endX1 = ovalX;
+        int endY1 = ovalY + (ovalHeight / 2);
+        g2d.drawLine(startX1, startY1, endX1, endY1);
+
+        // Desna linija za spoljašnji luk
+        int startX2 = x + width;
+        int startY2 = y + rectHeight;
+        int endX2 = ovalX + ovalWidth;
+        int endY2 = ovalY + (ovalHeight / 2);
+        g2d.drawLine(startX2, startY2, endX2, endY2);
+
+        // Leva linija za unutrašnji luk
+        int innerStartX1 = x + innerMargin;
+        int innerStartY1 = y + rectHeight + innerMargin;
+        int innerEndX1 = innerOvalX;
+        int innerEndY1 = innerOvalY + (innerOvalHeight / 2);
+        g2d.drawLine(innerStartX1, innerStartY1, innerEndX1, innerEndY1);
+
+        // Desna linija za unutrašnji luk
+        int innerStartX2 = x + width - innerMargin;
+        int innerStartY2 = y + rectHeight + innerMargin;
+        int innerEndX2 = innerOvalX + innerOvalWidth;
+        int innerEndY2 = innerOvalY + (innerOvalHeight / 2);
+        g2d.drawLine(innerStartX2, innerStartY2, innerEndX2, innerEndY2);
+
+        // Gornja linija za unutrasnji luk
+        int innerTopStartX = innerStartX1;
+        int innerTopStartY = innerStartY1;
+        int innerTopEndX = innerStartX2;
+        int innerTopEndY = innerStartY2;
+        g2d.drawLine(innerTopStartX, innerTopStartY, innerTopEndX, innerTopEndY);
+
         applySelectionBorder(g2d);
         g2d.setTransform(originalTransform);
     }
+
 }
